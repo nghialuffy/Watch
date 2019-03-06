112113626391eb5e6b600e836f0ccfdc714d0112
nghialuffy <34179162+nghialuffy@users.noreply.github.com>
2019-03-06 16:27:45 +0700

    Add files via upload

---
+++ WatchV2.0/WatchV2.0/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WatchV2._0
+{
+    partial class Dong_Ho
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dong_Ho));
+            this.LBGio = new System.Windows.Forms.Label();
+            this.LBGiay = new System.Windows.Forms.Label();
+            this.DMY = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PB1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LBGio
+            // 
+            this.LBGio.AutoSize = true;
+            this.LBGio.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBGio.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LBGio.Location = new System.Drawing.Point(87, 0);
+            this.LBGio.Name = "LBGio";
+            this.LBGio.Size = new System.Drawing.Size(199, 73);
+            this.LBGio.TabIndex = 0;
+            this.LBGio.Text = "00:00";
+            this.LBGio.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dong_Ho_MouseMove);
+            // 
+            // LBGiay
+            // 
+            this.LBGiay.AutoSize = true;
+            this.LBGiay.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBGiay.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LBGiay.Location = new System.Drawing.Point(271, 30);
+            this.LBGiay.Name = "LBGiay";
+            this.LBGiay.Size = new System.Drawing.Size(55, 37);
+            this.LBGiay.TabIndex = 1;
+            this.LBGiay.Text = "00";
+            this.LBGiay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dong_Ho_MouseMove);
+            // 
+            // DMY
+            // 
+            this.DMY.AutoSize = true;
+            this.DMY.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DMY.Location = new System.Drawing.Point(96, 75);
+            this.DMY.Name = "DMY";
+            this.DMY.Size = new System.Drawing.Size(269, 22);
+            this.DMY.TabIndex = 2;
+            this.DMY.Text = "Thứ Hai, 5 Tháng 11 Năm 2018";
+            this.DMY.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dong_Ho_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PB1
+            // 
+            this.PB1.Location = new System.Drawing.Point(3, 7);
+            this.PB1.Name = "PB1";
+            this.PB1.Size = new System.Drawing.Size(90, 90);
+            this.PB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB1.TabIndex = 3;
+            this.PB1.TabStop = false;
+            // 
+            // Dong_Ho
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(377, 106);
+            this.Controls.Add(this.PB1);
+            this.Controls.Add(this.DMY);
+            this.Controls.Add(this.LBGiay);
+            this.Controls.Add(this.LBGio);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Dong_Ho";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Watch";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Dong_Ho_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dong_Ho_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBGio;
+        private System.Windows.Forms.Label LBGiay;
+        private object fontDialog;
+        private System.Windows.Forms.Label DMY;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox PB1;
+    }
+}
+
---
+++ WatchV2.0/WatchV2.0/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WatchV2._0
+{
+    public partial class Dong_Ho : Form
+    {
+
+        public Dong_Ho()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Dong_Ho_Load(object sender, EventArgs e)
+        {
+            LBGio.Font = new Font("QUARTZ", 48, FontStyle.Bold);
+            LBGiay.Font = new Font("QUARTZ", 24, FontStyle.Bold);
+            Change_Picture();
+        }
+        string sHour;
+        string sMin;
+        string sSec;
+        string iDayofWeek;
+        string iDay;
+        string iMonth;
+        string iYear;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sHour = DateTime.Now.Hour.ToString();
+            sMin = DateTime.Now.Minute.ToString();
+            sSec = DateTime.Now.Second.ToString();
+
+            if (DateTime.Now.Hour < 10)
+                sHour = "0" + sHour;
+            if (DateTime.Now.Minute < 10)
+                sMin = "0" + sMin;
+            if (DateTime.Now.Second < 10)
+                sSec = "0" + sSec;
+            LBGio.Text = sHour + ":" + sMin;
+            LBGiay.Text = sSec;
+            iDayofWeek = DateTime.Now.DayOfWeek.ToString();
+            iDay = DateTime.Now.Day.ToString();
+            iMonth = DateTime.Now.Month.ToString();
+            iYear = DateTime.Now.Year.ToString();
+            if (iDayofWeek.ToLower() == "monday")
+                iDayofWeek = "Thứ Hai";
+            else if (iDayofWeek.ToLower() == "tuesday")
+                iDayofWeek = "Thứ Ba";
+            else if (iDayofWeek.ToLower() == "wednesday")
+                iDayofWeek = "Thứ Tư";
+            else if (iDayofWeek.ToLower() == "thursday")
+                iDayofWeek = "Thứ Năm";
+            else if (iDayofWeek.ToLower() == "friday")
+                iDayofWeek = "Thứ Sáu";
+            else if (iDayofWeek.ToLower() == "saturday")
+                iDayofWeek = "Thứ Bảy";
+            else if (iDayofWeek.ToLower() == "sunday")
+                iDayofWeek = "Chủ Nhật";
+            DMY.Text = iDayofWeek + ", " + iDay + " Tháng " + iMonth + " Năm " + iYear;
+            if (DateTime.Now.Hour == 6 && DateTime.Now.Minute == 0 && DateTime.Now.Second == 0)
+                Change_Picture();
+            if (DateTime.Now.Hour == 19 && DateTime.Now.Minute == 0 && DateTime.Now.Second == 0)
+                Change_Picture();
+        }
+        private void Change_Picture()
+        {
+            if (DateTime.Now.Hour > 6 && DateTime.Now.Hour < 19)
+            {
+                PB1.Image = Image.FromFile(@"sun.png");
+            }
+            else
+            {
+                PB1.Image = Image.FromFile(@"moon.png");
+            }
+        }
+
+        //Move
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        private void Dong_Ho_MouseMove(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+        }
+
+
+    }
+
+}
